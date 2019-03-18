4aab84a8202631867fbc3440fdb7f560f53806c9
Artjom Lell <artjom.lell@web.de>
2019-03-18 21:32:30 +0100

    added collector based methods

--- src/org/modernjava/stream/collector/CollectorMethods.java
+++ src/org/modernjava/stream/collector/CollectorMethods.java
@@ -3,14 +3,55 @@ package org.modernjava.stream.collector;
 import org.modernjava.testdata.transactions.Currency;
 import org.modernjava.testdata.transactions.Transaction;
 
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
+import java.util.*;
+
+import static java.util.stream.Collectors.*;
 
 public class CollectorMethods {
 
     static Map<Currency, List<Transaction>> getTransactionsByCurrencies(List<Transaction> transactionList) {
-        return transactionList.stream().collect(Collectors.groupingBy(Transaction::getCurrency));
+        return transactionList.stream().
+                collect(groupingBy(Transaction::getCurrency));
     }
+
+    // this method is only for learning collect stream method. Not useless for production
+    // could be useful by using with other collectors
+    static Long getCountOfAllTransactions(List<Transaction> transactionList) {
+        return transactionList.stream().
+                collect(counting());
+    }
+
+    // also can be replaced with IntStream sum method
+    static int getSumOfAllTransactionValues(List<Transaction> transactionList) {
+        return transactionList.stream().
+                collect(summingInt(Transaction::getSalesValue));
+        // another method: averaging...
+    }
+
+    // max transaction value by comparator
+    // can be replaced with max method of stream
+    static Transaction getTransactionWithHighestValue(List<Transaction> transactionList) {
+        Comparator<Transaction> transactionValueComparator = Comparator.comparingInt(Transaction::getSalesValue);
+
+        return transactionList.stream().
+                collect(maxBy(transactionValueComparator)).get();
+    }
+
+    // statistics about whole transaction values (average, min, max etc.)
+    static IntSummaryStatistics getTransactionStatistics(List<Transaction> transactionList) {
+        return transactionList.stream().
+                collect(summarizingInt(Transaction::getSalesValue));
+    }
+
+    // use reducing method from Collectors. Return value is Optional
+    // in same cases is better to use reducing instead of reduce Method of stream
+    // collect is mutable reduction. Is better for parallel processing
+    // collect() can only work with mutable result objects.
+    // reduce() is designed to work with immutable result objects. reduce is bad if objects are mutable
+    static Transaction getTransactionWithHighestValueByReducing(List<Transaction> transactionList) {
+        return transactionList.stream().
+                collect(reducing((d1, d2) -> d1.getSalesValue() > d2.getSalesValue() ? d1 : d2)).get();
+    }
+
 
 }
