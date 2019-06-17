363801e7ae31fb003d111c10a21f71199bf33a50
Artjom Lell <artjom.lell@web.de>
2019-06-17 17:39:23 +0200

    added performance measurement

---
+++ src/main/java/org/modernjava/stream/performance/ParallelStreamPerformance.java
@@ -0,0 +1,54 @@
+package org.modernjava.stream.performance;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+
+import java.util.concurrent.TimeUnit;
+import java.util.stream.LongStream;
+import java.util.stream.Stream;
+
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+@Fork(value = 2, jvmArgs = {"-Xms4G", "-Xmx4G"})
+public class ParallelStreamPerformance {
+
+	private static final long N = 10_000_000L;
+
+	@Benchmark
+	public long sequentialSum() {
+		return Stream.iterate(1L, i -> i + 1).limit(N).reduce(0L, Long::sum);
+	}
+
+	@Benchmark
+	public long iterativeSum() {
+		long result = 0;
+		for (long i = 1L; i <= N; i++) {
+			result += i;
+		}
+		return result;
+	}
+
+	@Benchmark
+	public long rangedSum() {
+		return LongStream.rangeClosed(1, N)
+				.reduce(0L, Long::sum);
+	}
+
+	@Benchmark
+	public long parallelRangedSum() {
+		return LongStream.rangeClosed(1, N)
+				.parallel()
+				.reduce(0L, Long::sum);
+	}
+
+
+/*
+	@TearDown(Level.Invocation)
+	public void tearDown() {
+		System.gc();
+	}
+*/
+}
